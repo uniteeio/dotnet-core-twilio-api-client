2e2eeb43594638aa90ca65f246de6dc2f965f2ea
Fabien PAITRY <fabien@ino-labs.fr>
2018-12-03 17:47:03 +0100

    Handle exception

--- TwilioApiClient/TwilioApiClient.cs
+++ TwilioApiClient/TwilioApiClient.cs
@@ -1,5 +1,7 @@
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Twilio;
+using Twilio.Exceptions;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Rest.Proxy.V1.Service.Session.Participant;
 using Twilio.Types;
@@ -25,18 +27,25 @@ namespace TwilioApiClient
                 throw new EmptyReceiverNumberException();
             }
 
-            var message = MessageResource.Create(
-                body: smsBody,
-                from: new PhoneNumber(_options.SenderPhoneNumber),
-                to: new PhoneNumber(receiverNumber)
-            );
-
-            if (message.Status == MessageInteractionResource.ResourceStatusEnum.Failed)
+            try
             {
-                  throw new SendSmsException();
-            }
+                var message = MessageResource.Create(
+                    body: smsBody,
+                    from: new PhoneNumber(_options.SenderPhoneNumber),
+                    to: new PhoneNumber(receiverNumber)
+                );
+                
+                if (message.Status == MessageInteractionResource.ResourceStatusEnum.Failed)
+                {
+                    throw new SendSmsException();
+                }
 
-            return message;
+                return message;
+            }
+            catch (ApiException e)
+            {
+                 throw new TwilioApiClientException(e.Message);
+            }
         }
     }
 }
